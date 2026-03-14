f1547019967c0eaf2d1709c5a1d1affb7b6a1094
kb8864 <yt7s858864@gmail.com>
2026-03-14 18:41:37 +0900

    スタックとヒープの問題

---
+++ JavaSilver対策/JavaSilver/src/java4_1/Item.java
@@ -0,0 +1,15 @@
+package java4_1;
+
+public class Item {
+	private  int num = 10;
+	//private static int num = 10;
+
+	public void setNum(int num) {
+		this.num = num;
+		
+	}
+	public int getNum() {
+		return this.num;
+		
+	}
+}
