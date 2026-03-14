2ea3ab068404cbd85b4ce91e8163e3a09126219f
kb8864 <yt7s858864@gmail.com>
2026-03-14 20:19:17 +0900

    メタスペースとstatic領域

---
+++ JavaSilver対策/JavaSilver/src/java4_5/Main.java
@@ -0,0 +1,14 @@
+package java4_5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sample.num = 10;
+		Sample s = new Sample();
+		Sample s2 = new Sample();
+		s.num += 10;
+		s2.num =30;
+		System.out.println(Sample.num);
+	}
+
+}
