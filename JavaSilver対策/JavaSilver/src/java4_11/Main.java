8334b547d6e3c3d4187b10f7eafcb4471f3eecee
kb8864 <yt7s858864@gmail.com>
2026-03-15 00:13:16 +0900

    voidで宣言しているのに、型が違う変数に入れようとしてコンパイルエラー

---
+++ JavaSilver対策/JavaSilver/src/java4_11/Main.java
@@ -0,0 +1,14 @@
+package java4_11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Sample s = new Sample();
+		String val = s.setValue("hello");
+		s.getValue(){
+			System.out.println(val);
+		}
+	}
+
+}
