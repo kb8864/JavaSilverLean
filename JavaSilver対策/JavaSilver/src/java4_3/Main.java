33a98d58840ec2bb24793ab865f4c94db73ce31b
kb8864 <yt7s858864@gmail.com>
2026-03-14 19:37:56 +0900

    がベージコレクションについて

--- JavaSilver対策/JavaSilver/src/java4_3/Main.java
+++ JavaSilver対策/JavaSilver/src/java4_3/Main.java
@@ -1,10 +1,10 @@
 package java4_3;
 
 public class Main {
-
+//Q。"NULLと表示させたいが下のコードであっているか？"
 	public static void main(String[] args) {
 		Object onj = Null;
-			System.out.println(obj);
+			System.out.println(obj);//A.あっていないコンパイルエラーになっちゃう
 	}
 
 }
---
+++ JavaSilver対策/JavaSilver/src/java4_4/Main.java
@@ -0,0 +1,14 @@
+package java4_4;
+
+public class Main {
+	public static void main(String[] args) {
+		Object a = new Object();
+		Object b = new Object();
+		Object c =  a;
+		
+		a = null;
+		b = null;
+
+	}
+
+}
