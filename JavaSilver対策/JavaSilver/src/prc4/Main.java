7eee77f03f1eb82d38c5825730c7274a1cc9adf5
kb8864 <yt7s858864@gmail.com>
2026-03-16 00:55:36 +0900

    static初期化子とstaticで修飾されたクラス変数

--- JavaSilver対策/JavaSilver/src/prc4/Main.java
+++ JavaSilver対策/JavaSilver/src/prc4/Main.java
@@ -1,10 +1,10 @@
 package prc4;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
+    public static void main(String[] args) {
+        System.out.println("--- main開始 ---");
+        Sample s1 = new Sample(); // 1台目を作る
+        System.out.println("--- 2台目 ---");
+        Sample s2 = new Sample(); // 2台目を作る
+    }
 }
