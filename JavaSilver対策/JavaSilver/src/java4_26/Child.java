076a1d3c1e078a7b5e46b6e720570f8b414afc3b
kb8864 <yt7s858864@gmail.com>
2026-03-25 16:58:44 +0900

    パッケージが違う」かつ「アクセス修飾子がない（デフォルト）」**ため、たとえ親子関係（継承）であってもアクセスできず、コンパイルエラ-

---
+++ JavaSilver対策/JavaSilver/src/java4_26/Child.java
@@ -0,0 +1,10 @@
+package java4_26;
+
+import ex26.Parent;
+
+public class Child extends Parent {
+	public static void main(String[] args) {
+		System.out.println(num);//Parentにアクセスはできるが、そのさきのメソッドはデフォルトのため、パッケージを跨いで使用することはできない
+	}
+
+}
